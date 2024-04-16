cf321720834453925c568b0ff8301319e93d0b99
anoban <2016sb117@univ.jfn.ac.lk>
2024-04-16 08:35:42 +0530

    modified:   .vscode/settings.json 	modified:   iris.cpp

--- iris.cpp
+++ iris.cpp
@@ -1,23 +1,28 @@
 #include <concepts>
+#include <fstream>
 #include <iostream>
 #include <vector>
 
-constexpr std::size_t NCHARS_SPECIESNAME { 20 }; // Iris-versicolor is the longest species name and it has 15 characters so 20 is okay :)
-constexpr std::size_t NROWS_CSV { 200 };         // iris.data has only 150 rows
+namespace iris {
 
-// columns : Sepal Length, Sepal Width, Petal Length, Petal Width & Species
+    constexpr std::size_t NCHARS_SPECIESNAME {
+        20
+    }; // Iris-versicolor is the longest species name and it has 15 characters so 20 is okay :)
+    constexpr std::size_t NROWS_CSV { 150 }; // iris.data has only 150 rows
 
-template<typename T> requires std::floating_point<T> struct record {
-        T    sepal_length {};
-        T    sepal_width {};
-        T    petal_length {};
-        T    petal_width {};
-        char species[NCHARS_SPECIESNAME] {};
+    // columns : Sepal Length, Sepal Width, Petal Length, Petal Width & Species
 
-        constexpr record() = default;
-};
+    template<typename T> requires std::floating_point<T> struct record {
+            T    sepal_length {};
+            T    sepal_width {};
+            T    petal_length {};
+            T    petal_width {};
+            char species[NCHARS_SPECIESNAME] {};
+    };
 
-template<typename T> requires std::floating_point<T> class Iris {
-    private:
-        std::array<record<T>, NROWS_CSV> rows {};
-};
+    template<typename T> static std::vector<record<T>> readIrisData(_In_ const wchar_t* const filename) noexcept {
+        std::ifstream ifile { L"./iris.data", std::ios::in };
+        std::istreambuf_iterator<char>(ifile.tellg());
+    }
+
+} // namespace iris
