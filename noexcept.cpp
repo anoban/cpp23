04190894f41b5f553cda289f6bb1cf337930801d
anoban <2016sb117@univ.jfn.ac.lk>
2024-05-11 09:43:09 +0530

    modified:   .vscode/tasks.json 	modified:   noexcept.cpp

--- noexcept.cpp
+++ noexcept.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <type_traits>
 
 template<typename T>
@@ -20,10 +21,18 @@ long double static constexpr cube(
     return ::power(x, 3);
 }
 
+template<typename T>
+typename std::enable_if<std::is_scalar<T>::value && !std::is_integral<T>::value, T>::type static consteval cube(
+    const T x, typename std::enable_if<std::is_floating_point<T>::value, T>::type = static_cast<T>(0)
+) noexcept {
+    return x * x * x;
+}
+
 int main() {
-    //
-    constexpr auto four { ::square(2.000) };
-    constexpr auto thousand { ::cube(10i16) };
-    constexpr auto sixtyfour { ::cube(::square(20 / 10)) };
-    return 0;
+    [[maybe_unused]] constexpr auto four { ::square(2.000) };
+    [[maybe_unused]] constexpr auto thousand { ::cube(10i16) };
+    [[maybe_unused]] constexpr auto sixtyfour { ::cube(::square(20 / 10)) };
+    [[maybe_unused]] constexpr auto nine { ::cube(3.000F) };
+    [[maybe_unused]] constexpr auto onetwofive { ::cube(5.000L) };
+    return EXIT_SUCCESS;
 }
