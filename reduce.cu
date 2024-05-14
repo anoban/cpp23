7b7ce98815a8ecc704064993e4a6b7f77e9ef85f
anoban <2016sb117@univ.jfn.ac.lk>
2024-05-14 18:15:57 +0530

    modified:   reduce.cu

--- reduce.cu
+++ reduce.cu
@@ -1,30 +1,44 @@
 // nvcc .\reduce.cu -std=c++20 -O3 -o .\reduce.exe
 
+#include <algorithm>
+#include <ctime>
 #include <numeric>
-#include <random>
+#include <ranges>
+#include <type_traits>
 #include <vector>
 
 static constexpr size_t NTHREADS { 1024 };
-static constexpr size_t NRANDOMS { NTHREADS * 1024 };
+static constexpr size_t NTASKSPERTHREAD { 200'000 };
+static constexpr size_t NRANDOMS { NTHREADS * NTASKSPERTHREAD };
 
 template<typename T> static constexpr typename std::enable_if<std::is_scalar<T>::value, size_t>::type memsize = sizeof(T) * NRANDOMS;
 
-__global__ void sum(_In_reads_(NRANDOMS) const int32_t* const numbers, _Inout_count_(NTHREADS) double* const sums) {
-    const auto index { threadIdx.x + threadIdx.y + threadIdx.z };
-    double     sum {};
-    for (unsigned i = index * NTHREADS; i < (index * NTHREADS) + NTHREADS; ++i) sum += numbers[i];
+template<typename scalar_in_t, typename scalar_out_t> requires std::is_scalar<scalar_in_t>::value && std::is_scalar<scalar_out_t>::value
+__global__ void sum(_In_count_(NRANDOMS) const scalar_in_t* const numbers, _Inout_count_(NTHREADS) scalar_out_t* const sums) {
+    const unsigned index { threadIdx.x + threadIdx.y + threadIdx.z };
+    scalar_out_t   sum {};
+    // cannot use STL in device code
+    // for (const auto& i : std::ranges::views::iota(index * NTASKSPERTHREAD, (index * NTASKSPERTHREAD) + NTASKSPERTHREAD)) sum += numbers[i];
+    const auto     start = index * NTASKSPERTHREAD;
+    for (auto i = start; i < start + NTASKSPERTHREAD; ++i) sum += numbers[i];
     sums[index] = sum;
     return;
 }
 
 // a fold kernel to sum the NTHREADS sums produced by each kernel
-__global__ void fold(_In_ const double* const sums, _Inout_ double* const result) {
-    double sum {};
+template<typename scalar_in_t, typename scalar_out_t>
+__global__ void fold(
+    _In_count_(NTHREADS) const scalar_in_t* const sums,
+    _Inout_count_(1) scalar_out_t* const          result,
+    typename std::enable_if<std::is_scalar_v<scalar_in_t>, scalar_in_t>::type   = static_cast<scalar_in_t>(0),
+    typename std::enable_if<std::is_scalar_v<scalar_out_t>, scalar_out_t>::type = static_cast<scalar_out_t>(0)
+) {
+    scalar_out_t sum {};
     for (size_t i {}; i < NTHREADS; ++i) sum += sums[i];
     *result = sum;
 }
 
-auto main() -> int {
+auto wmain() -> int {
     // std::random_device rdevice {};
     // std::mt19937_64    rengine { rdevice() };
 
@@ -32,33 +46,56 @@ auto main() -> int {
     std::vector<int32_t> randoms(NRANDOMS);
     std::generate(randoms.begin(), randoms.end(), rand);
 
+    int32_t* d_randoms {};
+    double*  d_sums {};
+    double*  d_total {};
+
+    double     device_sum {};
     const auto host_sum { std::accumulate(randoms.cbegin(), randoms.cend(), 0.0L) };
 
-    int32_t*    d_randoms {};
-    double*     d_sums {};
-    double*     d_total {};
     cudaError_t cuStatus {};
-
-    // TODO: Error handling
     cuStatus = ::cudaMalloc(&d_randoms, memsize<int32_t>);
+    if (cuStatus != cudaSuccess) {
+        ::fputws(L"Error :: cudaMalloc failed!", stderr);
+        return EXIT_FAILURE;
+    }
+
     cuStatus = ::cudaMalloc(&d_sums, sizeof(double) * NTHREADS);
+    if (cuStatus != cudaSuccess) {
+        ::fputws(L"Error :: cudaMalloc failed!", stderr);
+        goto PREMATURE_EXIT;
+    }
+
     cuStatus = ::cudaMalloc(&d_total, sizeof(double));
+    if (cuStatus != cudaSuccess) {
+        ::fputws(L"Error :: cudaMalloc failed!", stderr);
+        goto PREMATURE_EXIT;
+    }
 
     cuStatus = ::cudaMemcpy(d_randoms, randoms.data(), memsize<int32_t>, cudaMemcpyHostToDevice);
+    if (cuStatus != cudaSuccess) {
+        ::fputws(L"Error :: cudaMalloc failed!", stderr);
+        goto PREMATURE_EXIT;
+    }
+
     sum<<<1, NTHREADS>>>(d_randoms, d_sums);
     cuStatus = ::cudaDeviceSynchronize();
 
     fold<<<1, 1>>>(d_sums, d_total);
     cuStatus = ::cudaDeviceSynchronize();
 
-    double device_sum {};
     cuStatus = ::cudaMemcpy(&device_sum, d_total, sizeof(double), cudaMemcpyDeviceToHost);
+
+    ::wprintf_s(L"device sum :: %.5lf, host sum :: %.5Lf\n", device_sum, host_sum);
 
     ::cudaFree(d_randoms);
     ::cudaFree(d_sums);
     ::cudaFree(d_total);
-
-    ::wprintf_s(L"device sum :: %.5lf, host sum :: %.5Lf\n", device_sum, host_sum);
-
     return EXIT_SUCCESS;
+
+PREMATURE_EXIT:
+    ::cudaFree(d_randoms);
+    ::cudaFree(d_sums);
+    ::cudaFree(d_total);
+    return EXIT_FAILURE;
 }
