cbecc31ccbc019bfa4f1f1f93f764c240bd9de66
anoban <2016sb117@univ.jfn.ac.lk>
2024-05-14 12:40:13 +0530

    modified:   reduce.cu

--- reduce.cu
+++ reduce.cu
@@ -1,3 +1,5 @@
+// nvcc .\reduce.cu -std=c++20 -O3 -o .\reduce.exe
+
 #include <numeric>
 #include <random>
 #include <vector>
@@ -7,7 +9,7 @@ static constexpr size_t NRANDOMS { NTHREADS * 1024 };
 
 template<typename T> static constexpr typename std::enable_if<std::is_scalar<T>::value, size_t>::type memsize = sizeof(T) * NRANDOMS;
 
-__global__ void sum(_In_reads_(NRANDOMS) const int64_t* const numbers, _Inout_count_(NTHREADS) double* const sums) {
+__global__ void sum(_In_reads_(NRANDOMS) const int32_t* const numbers, _Inout_count_(NTHREADS) double* const sums) {
     const auto index { threadIdx.x + threadIdx.y + threadIdx.z };
     double     sum {};
     for (unsigned i = index * NTHREADS; i < (index * NTHREADS) + NTHREADS; ++i) sum += numbers[i];
@@ -23,25 +25,26 @@ __global__ void fold(_In_ const double* const sums, _Inout_ double* const result
 }
 
 auto main() -> int {
-    std::random_device rdevice {};
-    std::mt19937_64    rengine { rdevice() };
+    // std::random_device rdevice {};
+    // std::mt19937_64    rengine { rdevice() };
 
-    std::vector<int64_t> randoms(NRANDOMS);
-    std::generate(randoms.begin(), randoms.end(), rengine);
+    srand(time(nullptr));
+    std::vector<int32_t> randoms(NRANDOMS);
+    std::generate(randoms.begin(), randoms.end(), rand);
 
-    const auto host_sum { std::accumulate(randoms.cbegin(), randoms.cend(), 0.0L, std::plus<int64_t> {}) };
+    const auto host_sum { std::accumulate(randoms.cbegin(), randoms.cend(), 0.0L) };
 
-    int64_t*    d_randoms {};
+    int32_t*    d_randoms {};
     double*     d_sums {};
     double*     d_total {};
     cudaError_t cuStatus {};
 
     // TODO: Error handling
-    cuStatus = ::cudaMalloc(&d_randoms, memsize<int64_t>);
+    cuStatus = ::cudaMalloc(&d_randoms, memsize<int32_t>);
     cuStatus = ::cudaMalloc(&d_sums, sizeof(double) * NTHREADS);
     cuStatus = ::cudaMalloc(&d_total, sizeof(double));
 
-    cuStatus = ::cudaMemcpy(d_randoms, randoms.data(), memsize<int64_t>, cudaMemcpyHostToDevice);
+    cuStatus = ::cudaMemcpy(d_randoms, randoms.data(), memsize<int32_t>, cudaMemcpyHostToDevice);
     sum<<<1, NTHREADS>>>(d_randoms, d_sums);
     cuStatus = ::cudaDeviceSynchronize();
 
