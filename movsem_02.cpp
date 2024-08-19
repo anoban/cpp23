092d76f0adbf24d397e9a17eae81f3b3f1bdc381
anoban <2016sb117@univ.jfn.ac.lk>
2024-08-19 16:16:31 +0530

    modified:   .vscode/settings.json 	modified:   .vscode/tasks.json 	modified:   movsem_02.cpp 	modified:   sstring

--- movsem_02.cpp
+++ movsem_02.cpp
@@ -3,7 +3,8 @@
 
 #include <sstring>
 
-static inline ::sstring skyfall() throw() {
+static inline ::sstring skyfall() __noexcept__ {
+    // NOLINTNEXTLINE(modernize-return-braced-init-list)
     return ::sstring("Skyfall is where we start, a thousand miles and poles apart, where worlds collide and days are dark");
 }
 
@@ -14,15 +15,15 @@ int main() {
     ::sstring       rust_style = ::sstring::with_capacity(7 * MiB);
 
     const ::sstring jbond("I've drowned and dreamt this moment.... so overdue I owe them................");
-    ::puts(jbond.c_str());
+    std::cout << jbond << '\n';
 
     sstring adele;       // default construction
     adele = ::skyfall(); // copy assignment in C++03, move assignment in C++11 and later
 
-    ::puts(adele.c_str());
+    std::cout << adele << '\n';
 
     const sstring aaaaa('A', 50);
-    ::puts(aaaaa.c_str());
+    std::cout << aaaaa << '\n';
 
     return EXIT_SUCCESS;
 }
