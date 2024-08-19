bf5c0b53e8f5dbca0bbbc5b6fd086b9283321836
anoban <2016sb117@univ.jfn.ac.lk>
2024-08-19 15:49:28 +0530

    modified:   macros.cpp 	modified:   sstring

--- macros.cpp
+++ macros.cpp
@@ -1,27 +1,33 @@
 #include <cstdio>
 #include <cstdlib>
 
-#define __helper__(expanded_macro) L##expanded_macro // token paste
-#define to_wstring(macro)          __helper__(macro)
+// https://learn.microsoft.com/en-us/cpp/preprocessor/preprocessor-experimental-overview?view=msvc-170
+
+#define __helper__(expanded_macro) L##expanded_macro // token paster
+#define to_wstring(macro)          __helper__(macro) // user facing macro expander
 
 #define NAME                       "ANOBAN"
 
 static constexpr auto Anoban { to_wstring(NAME) };
 static constexpr auto Dell { to_wstring("DELL") };
 
-#define FUNC_FULL_NAME to_wstring(__FUNCSIG__)
+#if (defined __llvm__ && defined __clang__) || (defined __GNUC__ && defined __GNUG__)
+    #define FUNC_FULL_NAME to_wstring(__PRETTY_FUNCTION__) // for LLVM and g++
+#elif defined _MSC_VER && defined _MSC_FULL_VER
+    #if !defined(_MSVC_TRADITIONAL) || (_MSVC_TRADITIONAL) // not defined or defined to be 0
+        #error Use the new preprocessor!
+    #endif
+    #define FUNC_FULL_NAME to_wstring(__FUNCSIG__) // for MSVC
+#endif
+
+static constexpr decltype(auto) signature() noexcept { return FUNC_FULL_NAME; }
 
 auto wmain() -> int {
-    constexpr auto main { to_wstring(__FUNCSIG__) };
-    ::_putws(main);
     ::_putws(Anoban);
     ::_putws(Dell);
 
     ::_putws(FUNC_FULL_NAME);
-
-#ifdef __llvm__
-    ::puts(__PRETTY_FUNCTION__);
-#endif
+    ::_putws(::signature());
 
     return EXIT_SUCCESS;
 }
