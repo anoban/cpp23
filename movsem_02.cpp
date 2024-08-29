39159bc77bac1a98072fbb003986293d3af6cf14
anoban <2016sb117@univ.jfn.ac.lk>
2024-08-29 19:11:26 +0530

    modified:   movsem_02.cpp 	modified:   sstring

--- movsem_02.cpp
+++ movsem_02.cpp
@@ -1,7 +1,9 @@
 // g++ movsem_02.cpp -Wall -Wextra -Wpedantic -O3 -std=c++xx -o movsem_02.exe -I ./
 // <cstdint> requires C++11 and later with g++ hence won't compile with -std=c++03
 
+#include <algorithm>
 #include <sstring>
+#include <string>
 
 static inline ::sstring skyfall() noexcept {
     // NOLINTNEXTLINE(modernize-return-braced-init-list)
@@ -16,7 +18,7 @@ int main() {               // NOLINT(bugprone-exception-escape)
     const ::sstring empty; // default construction
     ::sstring       crab_style = ::sstring::with_capacity(7 * MiB);
 
-    const ::sstring jbond("I've drowned and dreamt this moment.... so overdue I owe them................");
+    ::sstring jbond("I've drowned and dreamt this moment.... so overdue I owe them................");
     std::cout << jbond << '\n';
 
     sstring adele;       // default construction
@@ -32,5 +34,24 @@ int main() {               // NOLINT(bugprone-exception-escape)
 
     const ::sstring no_time_to_die = ::no_time_to_die(); // move construction in C++11 and later, copy construction in in C++03 and before
     ::puts(no_time_to_die.c_str());
+
+    jbond += " swept away I'm stooooolennnnnn..... let the sky fall.... when it crumbles we'll stand tall and face it all together...!";
+    ::puts(jbond.c_str());
+
+#if (__cplusplus >= 201103L)
+
+    for (const auto& c : jbond) {
+        ::putchar(c);
+        ::putchar('\n');
+    }
+
+    std::transform(jbond.begin(), jbond.end(), jbond.begin(), ::toupper);
+    ::puts(jbond.c_str());
+
+#endif
+
+    const std::string james(jbond.cbegin(), jbond.cend()); // will work with -std=c++98 too :)
+    std::cout << james << '\n';
+
     return EXIT_SUCCESS;
 }
