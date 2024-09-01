d141f7d575e9f99b53dd2ad7bcd6161d40c759f1
anoban <2016sb117@univ.jfn.ac.lk>
2024-09-01 21:08:30 +0530

    modified:   vs2008/main.cpp 	modified:   vs2008/sstring

--- vs2008/main.cpp
+++ vs2008/main.cpp
@@ -2,6 +2,11 @@
 #include <sstring>
 #include <algorithm>
 
+static inline char __stdcall sentence_case(const char* const str) throw(){
+    if(*str == ' ' && *(str + 1) >= 97 && *(str + 1) <= 122) return (*str) - 32;
+    return *str;
+}
+
 int main(){
     const ::sstring brennan = "I remember that time we were sippin' on wine";
     std::cout << brennan << '\n';
@@ -13,6 +18,9 @@ int main(){
 
     ::sstring lithe = "heard you want a piece of the pie huh?";
     lithe += " all the smokin' made me tired! Bitch I see the legacy , It's qiet!\n";
+    std::cout << lithe;    
+
+    for(unsigned i = 0; i < lithe.length(); ++i) lithe.at(i) = ::sentence_case(lithe.c_str() + i);
     std::cout << lithe;    
 
     std::cout << "__cplusplus is " << __cplusplus << '\n';
