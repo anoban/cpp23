fb0aef5b7c684cf869535405914d24c86a8c9299
anoban <2016sb117@univ.jfn.ac.lk>
2024-09-03 02:51:24 +0530

    modified:   vs2008/main.cpp 	modified:   vs2008/vs2008.vcproj

--- vs2008/main.cpp
+++ vs2008/main.cpp
@@ -1,27 +1,27 @@
 #define __SSTRING_PRINT_METHOD_SIGNATURES_ON_CALL__ 1
-#include <sstring>
 #include <algorithm>
+#include <sstring>
 
-static inline char __stdcall sentence_case(const char* const str) throw(){
-    if(*str == ' ' && *(str + 1) >= 97 && *(str + 1) <= 122) return (*str) - 32;
-    return *str;
+static inline void __stdcall sentence_case(::sstring& str) throw() {
+    for (unsigned long i = 0; i < str.length(); ++i)    // my bad :(
+        if ((str.at(i) == ' ') && (str.at(i + 1) >= 97) && (str.at(i + 1) <= 122)) str.at(i + 1) -= 32;
 }
 
-int main(){
+int main() {
     const ::sstring brennan = "I remember that time we were sippin' on wine";
     std::cout << brennan << '\n';
 
     ::sstring copy = brennan;
-    std::cout << copy << '\n';    
+    std::cout << copy << '\n';
     std::transform(copy.cbegin(), copy.cend(), copy.begin(), ::toupper);
-    std::cout << copy << '\n';    
+    std::cout << copy << '\n';
 
-    ::sstring lithe = "heard you want a piece of the pie huh?";
-    lithe += " all the smokin' made me tired! Bitch I see the legacy , It's qiet!\n";
-    std::cout << lithe;    
+    ::sstring lithe  = "heard you want a piece of the pie huh?";
+    lithe           += " all the smokin' made me tired! Bitch I see the legacy, It's qiet!\n";
+    std::cout << lithe;
 
-    for(unsigned i = 0; i < lithe.length(); ++i) lithe.at(i) = ::sentence_case(lithe.c_str() + i);
-    std::cout << lithe;    
+    ::sentence_case(lithe);
+    std::cout << lithe;
 
     std::cout << "__cplusplus is " << __cplusplus << '\n';
     ::getchar();
