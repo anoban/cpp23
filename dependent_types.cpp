abfe780dcbbaf4ad85a2809d4df720f94aaf0cc4
anoban <2016sb117@univ.jfn.ac.lk>
2024-09-09 18:49:50 +0530

    modified:   .clang-tidy 	modified:   .clangd 	new file:   dependent_types.cpp 	modified:   sstring

---
+++ dependent_types.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <type_traits>
+
+template<bool predicate, class T> struct enablde_if final { };
+
+template<class T> struct enablde_if<true, T> final {
+        static constexpr bool value { true };
+        using type = T;
+};
+
+// alias template
+template<bool value, class T> using enable_if_t                        = typename ::enablde_if<value, T>::type;
+
+// alias templates DO NOT allow partial or explicit specializations!
+template<template<class> class conditional, class T> using enable_if_t = typename ::enablde_if<conditional<T>::value, T>::type;
+
+// variable template
+template<bool value, class T> static constexpr bool enable_if_v        = ::enablde_if<value, T>::value;
+
+auto wmain() -> int {
+    constexpr ::enable_if_t<false, float> invalid {};
+    return EXIT_SUCCESS;
+}
