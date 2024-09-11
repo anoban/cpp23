cff9884bf58370dee5a3d633b968b5dec5f525e1
anoban <2016sb117@univ.jfn.ac.lk>
2024-09-11 13:29:41 +0530

    modified:   dependent_types.cpp 	modified:   rvalueref.cpp 	modified:   sstring

--- dependent_types.cpp
+++ dependent_types.cpp
@@ -17,7 +17,14 @@ template<bool value, class T> using enable_if_t                 = typename ::ena
 // variable template
 template<bool value, class T> static constexpr bool enable_if_v = ::enablde_if<value, T>::value;
 
+template<template<typename> typename predicate, typename T, bool _is_true = predicate<T>::value> struct predicate_if final { };
+
+template<template<typename> typename predicate, typename T> struct predicate_if<predicate, T, true> final {
+        using type = T;
+};
+
 auto wmain() -> int {
-    [[maybe_unused]] constexpr ::enable_if_t<true, float> valid {};
+    [[maybe_unused]] constexpr ::enable_if_t<true, float>                      valid {};
+    [[maybe_unused]] constexpr ::predicate_if<std::is_arithmetic, float>::type okay {};
     return EXIT_SUCCESS;
 }
--- rvalueref.cpp
+++ rvalueref.cpp
@@ -1,12 +1,16 @@
 #include <concepts>
 #include <cstdlib>
 #include <iostream>
+#include <numbers>
 
 // references and pointers do not allow type punned bindings!
 
-[[maybe_unused]] static float eps { 2.71828182845905 };
+[[maybe_unused]] static float eps { 2.71828182845905 }; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
 
 template<class T> requires std::is_arithmetic_v<T> class wrapper final {
+    public:
+        using value_type = T;
+
     private:
         T _value;
         template<class _Ty> requires std::is_arithmetic_v<_Ty>
@@ -25,17 +29,23 @@ template<class T> requires std::is_arithmetic_v<T> class wrapper final {
         constexpr ~wrapper() noexcept { _value = 0; }
 
         // conversion operator
-        //  template<class _Ty> requires std::is_arithmetic_v<_Ty> operator _Ty() noexcept {
-        //      ::_putws(__FUNCTIONW__);
-        //      return static_cast<_Ty>(_value);
-        //  }
+        template<class _Ty> requires std::is_arithmetic_v<_Ty> operator _Ty() noexcept {
+            ::_putws(L"template<class _Ty> requires std::is_arithmetic_v<_Ty> operator _Ty() noexcept");
+            return static_cast<_Ty>(_value);
+        }
 
-        // will a converting ctor make the above operation ambiguous??
+        // when a templated copy ctor is available, the compiler uses it instead of a conversion operator
+        // because technically a use of conversion operator as defined here requires invocation of the copy ctor (as defined here) down the line
+        // with the acquired wrapped value type
         template<class _Ty> requires std::is_arithmetic_v<_Ty>
         constexpr wrapper(const wrapper<_Ty>& other) noexcept : _value { static_cast<T>(other._value) } {
-            ::_putws(__FUNCTIONW__);
+            ::_putws(L"template<class _Ty> requires std::is_arithmetic_v<_Ty> constexpr wrapper(const wrapper<_Ty>& other) noexcept");
         }
 };
+
+template<std::floating_point T> static constexpr T e_v = static_cast<T>(2.71828182845905L);
+
+[[nodiscard]] static inline constexpr double pi() noexcept { return 3.14159265358979; }
 
 auto wmain() -> int {
     // try binding a eps to a double&
@@ -47,12 +57,19 @@ auto wmain() -> int {
     std::wcout << std::hex << std::uppercase;
     std::wcout << std::addressof(eps) << L'\n' << std::addressof(valid) << L'\n' << std::addressof(valid_too) << L'\n';
 
-    std::wcout << L"\n\n";
+    std::wcout << L'\n';
 
     auto                      pi { wrapper { 3.14159265358979L } };
     const wrapper<float>&     okay { pi };
     wrapper<unsigned short>&& okay_too { pi };
     std::wcout << std::addressof(pi) << L'\n' << std::addressof(okay) << L'\n' << std::addressof(okay_too) << L'\n';
 
+    const unsigned short& truncation = std::numbers::pi_v<double>;
+
+    const float*       ptr           = &std::numbers::pi_v<float>; // works
+    const double*      _ptr          = &2.71828182845905;          //  error: cannot take the address of an rvalue of type 'double'
+    const long double* __ptr         = &::e_v<long double>; // works, seems like we can take the address of constexpr'd variable templates
+
+    const double* _pi                = &::pi(); // WOW
     return EXIT_SUCCESS;
 }
