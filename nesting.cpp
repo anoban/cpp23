71118e4cb642afab1a5ae3143f772ca4950f5a54
anoban <2016sb117@univ.jfn.ac.lk>
2024-09-12 14:24:52 +0530

    modified:   nesting.cpp

--- nesting.cpp
+++ nesting.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cstdlib>
+#include <numbers>
 #include <type_traits>
 
 template<class T, class = std::enable_if<std::is_integral_v<T>, T>::type> class Integral final {
@@ -20,7 +21,7 @@ template<class T, class = std::enable_if<std::is_integral_v<T>, T>::type> class 
 
         // conversion operator, to convert to arithmetic types
         template<class U, bool is_unsigned = std::is_unsigned<U>::value> requires std::is_arithmetic_v<U>
-        constexpr operator U() const noexcept {
+        [[nodiscard]] constexpr operator U() const noexcept {
             return static_cast<U>(_value);
         }
 
@@ -30,7 +31,6 @@ template<class T, class = std::enable_if<std::is_integral_v<T>, T>::type> class 
 };
 
 // delete the conversion operator of Integral template when the required type is unsigned
-template<class _Ty> template<class unsigned_type> Integral<_Ty> unsigned_type<unsigned_type, true>() noexcept = delete;
 
 template<class T, class = std::enable_if<std::is_floating_point_v<T>, T>::type> class Floating final {
     private:
@@ -47,10 +47,36 @@ template<class T, class = std::enable_if<std::is_floating_point_v<T>, T>::type> 
 
         constexpr explicit Floating(const T& init) noexcept : _value(init) { }
         constexpr ~Floating() = default;
+
+        // conversion operator
+        template<class U, bool is_unsigned = std::is_unsigned_v<U>> requires std::is_arithmetic_v<U>
+        [[nodiscard]] constexpr operator U() const noexcept {
+            return static_cast<U>(_value);
+        }
 };
 
 auto wmain() -> int {
     constexpr ::Integral<short>          five46 { 546 };
     [[maybe_unused]] constexpr long long fivefour6 { five46 }; // implicit invocation of the conversion operator
+
+    constexpr double pi { ::Floating { ::std::numbers::pi_v<float> } };
     return EXIT_SUCCESS;
 }
+
+// function templates does not allow partial specializations
+
+template<template<typename> class unary_predicate, typename... TList> struct any_of final {
+        static constexpr bool value = (... || unary_predicate<TList>::value);
+};
+
+template<typename... TList>
+[[nodiscard]] long double consteval sum(
+    const bool has_unsigned_types = ::any_of<std::is_unsigned, TList...>::value, const TList&... args
+) noexcept {
+    return (... + args);
+}
+
+static_assert(::sum(10, 11, 12, 13, 14, 15, 16) == 91); // :) cool
+
+// can't do it! function template partial specialization is not allowed
+// template<typename... TList> [[nodiscard]] long double consteval sum<TList..., true>(const TList&... args) noexcept { return (... + args); }
