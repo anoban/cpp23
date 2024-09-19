c866799cd2d122cd7ac49151eca4fb34329519e9
anoban <2016sb117@univ.jfn.ac.lk>
2024-09-19 06:26:48 +0530

    modified:   arrayref.cpp 	modified:   sstring

--- arrayref.cpp
+++ arrayref.cpp
@@ -1,6 +1,7 @@
 #include <cctype>
 #include <cstdio>
 #include <cstdlib>
+#include <type_traits>
 
 // signature of an array reference is T (&identifier)[length]
 
@@ -28,6 +29,14 @@ static constexpr inline void square(_Inout_ T (&array)[__len]) noexcept requires
     for (size_t i = 0; i < __len; ++i) array[i] *= array[i];
 }
 
+template<class _Ty0, class _Ty1, size_t len>
+static typename std::enable_if<std::is_arithmetic_v<_Ty0> && std::is_arithmetic_v<_Ty1>, _Ty0 (&)[len]>::type operator+(
+    _Ty0 (&left)[len], const _Ty1 (&right)[len]
+) noexcept {
+    for (size_t i = 0; i < len; ++i) left[i] += right[i];
+    return left;
+}
+
 auto wmain() -> int {
     double (*const ptrcube)(const float&) noexcept = cube;
     //  ptrcube = quad;
@@ -49,6 +58,11 @@ auto wmain() -> int {
     for (const auto& e : array) ::wprintf_s(L"%6u\n", e);
 
     // ::wprintf_s(L"%S\n", ::capitalize("wayyyy downnn we gooooooooo!"));
+
+    int    integers[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    double reals[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+    integers + reals;
 
     return EXIT_SUCCESS;
 }
