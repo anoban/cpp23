51509d28df046962f009a4346bd0752982810940
anoban <2016sb117@univ.jfn.ac.lk>
2024-09-30 04:55:46 +0530

    modified:   googletest/src/gtest-all.cc 	new file:   gtest.ps1 	modified:   gtestdemo.cpp

---
+++ gtest.ps1
@@ -0,0 +1,64 @@
+$cfiles = [System.Collections.ArrayList]::new()
+$unrecognized = [System.Collections.ArrayList]::new()
+
+foreach ($arg in $args) {
+    if (($arg -clike "*.cpp") -or ($arg -clike "*.cc")){
+        [void]$cfiles.Add($arg.ToString().Replace(".\", ""))
+    }
+    else {
+        [void]$unrecognized.Add($arg)
+    }
+}
+
+if ($unrecognized.Count -ne 0) {
+    Write-Error "Incompatible files passed for compilation: ${unrecognized}"
+    Exit 1
+}
+
+$cflags = @(
+    "./googletest/src/gtest-all.cc",
+    "/arch:AVX512",
+    "/diagnostics:caret",
+    "/DNDEBUG",
+    "/EHsc",
+    "/F0x10485100",
+    "/favor:INTEL64",
+    "/fp:strict",
+    "/fpcvt:IA",
+    "/GL",
+    "/Gw",
+    "/I./googletest/",
+    "/I./googletest/include/",
+    "/jumptablerdata",
+    "/MP",
+    "/MT",
+    "/O2",
+    "/Ob3",
+    "/Oi",
+    "/Ot",
+    "/Qpar",
+    "/std:c++20",
+    "/TP",
+    "/Wall",
+    "/wd4514",      # removed unreferenced inline function
+    "/wd4710",      # not inlined
+    "/wd4711",      # selected for inline expansion
+    "/wd4820",      # struct padding
+    "/wd4623",
+    "/wd4625",
+    "/wd4626",
+    "/wd4668",
+    "/wd5026",
+    "/wd5027",
+    "/Zc:__cplusplus",
+    "/Zc:preprocessor",
+    "/link /DEBUG:NONE"
+)
+
+Write-Host "cl.exe ${cfiles} ${cflags}" -ForegroundColor Cyan
+cl.exe $cfiles $cflags
+
+# If cl.exe returned 0 (True), (i.e if the compilation succeeded,)
+if ($? -eq $True){
+        Remove-Item "*.obj" -Force
+}
--- gtestdemo.cpp
+++ gtestdemo.cpp
@@ -4,11 +4,68 @@ template<class... TList> [[nodiscard]] static constexpr long double sum(const TL
 
 TEST(SUM, ZEROES) {
     //
-    EXPECT_DOUBLE_EQ(::sum(0.000, 0LL, 0U, 0.0000F, 0, 0Ui16), 0.00000);
+    EXPECT_DOUBLE_EQ(::sum(0.000, 0LL, 0U, 0.0000F, 0, '\0'), 0.00000);
 }
 
-auto main() -> int {
-    //
+class Fixture : public testing::Test {
+    protected:
+        float _value_0;
+        float _value_1;
 
-    return EXIT_SUCCESS;
+        // inline Fixture() noexcept : testing::Test(), _value_0(10.0000), _value_1(11.00000) { }
+
+        inline void SetUp() noexcept override {
+            _value_0 = 100.000;
+            _value_1 = 110.000;
+        }
+
+        // inline ~Fixture() noexcept override = default;
+
+        inline void TearDown() noexcept override { _value_0 = _value_1 = 0.00000; }
+
+        inline void SayHi() noexcept { ::puts("Hi there!"); }
+};
+
+TEST_F(Fixture, IS_TEN_AND_ELEVEN) {
+    EXPECT_FLOAT_EQ(_value_0, 100.0000);
+    EXPECT_FLOAT_EQ(_value_1, 110.0000);
+    this->SayHi();
+    EXPECT_NE(_value_0, 10.0000);
+    EXPECT_NE(_value_1, 11.0000);
+}
+
+// The fixture for testing class Foo.
+class FooTest : public testing::Test {
+    protected:
+        // You can remove any or all of the following functions if their bodies would
+        // be empty.
+
+        FooTest() noexcept {
+            // You can do set-up work for each test here.
+        }
+
+        ~FooTest() noexcept override {
+            // You can do clean-up work that doesn't throw exceptions here.
+        }
+
+        // If the constructor and destructor are not enough for setting up
+        // and cleaning up each test, you can define the following methods:
+
+        void SetUp() noexcept override {
+            // Code here will be called immediately after the constructor (right
+            // before each test).
+        }
+
+        void TearDown() noexcept override {
+            // Code here will be called immediately after each test (right
+            // before the destructor).
+        }
+
+        // Class members declared here can be used by all tests in the test suite
+        // for Foo.
+};
+
+auto main() -> int {
+    testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
 }
