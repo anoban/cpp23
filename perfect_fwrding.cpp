cd4de6afdee158c44819081241709589486eb5a1
anoban <2016sb117@univ.jfn.ac.lk>
2024-10-10 20:44:01 +0530

    modified:   perfect_fwrding.cpp 	new file:   perffrwd.cpp 	modified:   sstring 	modified:   ssvector.cpp

--- perfect_fwrding.cpp
+++ perfect_fwrding.cpp
@@ -1,20 +1,30 @@
-#include <type_traits>
-#include <cstring>
+#define __SSTRING_PRINT_METHOD_SIGNATURES_ON_CALL__
 #include <algorithm>
-#include <string>
+#include <cstring>
+#include <sstring>
+#include <type_traits>
 
-static inline void capitalize(_Inout_ std::string& string) noexcept {	// use of a mutable reference as an in-out function argument
-	std::transform(string.begin(), string.end(), string.begin(), ::toupper);
+static inline void capitalize(_Inout_ ::sstring& string) noexcept { // use of a mutable reference as an in-out function argument
+    std::transform(string.begin(), string.end(), string.begin(), ::toupper);
 }
 
-template<typename _TyChar, typename = typename std::enable_if<std::is_same<_TyChar, char>::value || std::is_same<_TyChar, wchar_t>::value, _TyChar>::type>
-static inline void print(_In_ const std::basic_string<_TyChar>& string) noexcept {
-	::wprintf_s(std::is_same<_TyChar, char>::value ? L"%S\n" : L"%s\n", string.c_str());
-}
+static inline void print(_In_ const ::sstring& string) noexcept { ::printf_s("%s\n", string.c_str()); }
+
+static inline ::sstring get() noexcept { return ::sstring("perfect forwarding"); }
 
 auto main() -> int {
-	std::string name {"Anoban"};
-	::capitalize(name);
-	::print(name);
-	return EXIT_SUCCESS;
+    ::sstring name { "Anoban" };
+    ::capitalize(name);
+    ::print(name);
+
+    ::sstring laptop { "Alienware" };
+    static_cast<::sstring&&>(laptop) = "Thinkpad"; // this is valid C++ because
+    auto&& xvalue { ::get() };
+    xvalue = "still perfect :)"; // needs to be valid since these two are semantically equivalent
+
+    auto* ptr { &::get() };                           // error because we cannot take the address of an unmaterialized temporary (prvalue)
+                                                      // or rvalues in general
+    auto* _ptr { &static_cast<::sstring&&>(laptop) }; // error for the same reason because std::move here is just static_cast<::sstring&&>()
+
+    return EXIT_SUCCESS;
 }
--- ssvector.cpp
+++ ssvector.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     std::vector<sstring> collect;
-    collect.emplace_back("Hello there!");
+    collect.emplace_back("Hello there!"); // std::vector::emplace_back is a C++11 feature
     for (std::vector<sstring>::const_iterator it = collect.begin(), end = collect.end(); it != end; ++it) std::cout << *it << '\n';
     return EXIT_SUCCESS;
 }
