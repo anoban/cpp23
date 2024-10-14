9b8b1ce72c39bc93cbcaefc1d5f5a51e5494ef78
anoban <2016sb117@univ.jfn.ac.lk>
2024-10-14 07:08:32 +0530

    modified:   overloadres.cpp

--- overloadres.cpp
+++ overloadres.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
+#include <sstring>
 
 #ifdef __GNUG__
     #define __FUNCSIG__ __PRETTY_FUNCTION__
@@ -31,12 +32,26 @@ namespace overloads {
 
 } // namespace overloads
 
-auto main() -> int {
+namespace recursion {
+    [[nodiscard]] static ::sstring&& append(_Inout_ ::sstring&& string, _In_ ::sstring&& annexure, _In_ const unsigned times) noexcept {
+        string += annexure;
+        if (!times) return std::move(string);
+        return append(std::move(string), std::move(annexure), times - 1);
+    }
+} // namespace recursion
+
+auto wmain() -> int {
     constexpr float pi { M_PI };
     float           eps { M_E };
+    const float&&   rref { M_SQRT2 }; // temporary materialization
+
     overloads::func(12.98769458); // calls the rvalue reference overload
-    // clang and g++ are okay with this but MSVC errs saying this is an ambiguous call
+    // clang and g++ are okay with this but MSVC overload resolution errs saying this is an ambiguous call
+
     overloads::func(pi);  // calls the const lvalue reference overload
     overloads::func(eps); // calls the non-const lvalue reference overload
+    overloads::func(rref);
+    overloads::func(std::move(pi)); // calls the const rvalue reference overload
+
     return EXIT_SUCCESS;
 }
