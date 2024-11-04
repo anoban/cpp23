7a9d0939d9c6a1b5f33fdd2d6fb8e02d35660363
anoban <2016sb117@univ.jfn.ac.lk>
2024-11-04 14:17:40 +0530

    modified:   strliterals.cpp 	new file:   test.ps1

--- strliterals.cpp
+++ strliterals.cpp
@@ -1,4 +1,4 @@
-// can we differentiate const char (&)[] from const char* in C++
+// can we differentiate const char (&)[] from const char* in C++???
 
 #include <cstdio>
 #include <cstdlib>
@@ -8,8 +8,7 @@
 #include <source_location>
 #include <unix>
 
-template<size_t length>
-static inline void print(
+template<size_t length> static inline void print(
     _In_ const char (&string)[length], _In_opt_ const std::source_location& sloc = std::source_location::current()
 ) noexcept {
     ::printf_s("%s invoked @ line %u\n", __PRETTY_FUNCTION__, sloc.line()); // NOLINT(cppcoreguidelines-pro-type-vararg)
@@ -37,12 +36,11 @@ auto main() -> int {
     const auto fsize { std::filesystem::file_size(R"(./romeojuliet.txt)") };
     printf_s("std::ifstream %lld, std::filesystem %llu \n", nbytes, fsize); // NOLINT(cppcoreguidelines-pro-type-vararg)
 
-    char* str = new (std::nothrow) char[nbytes];
-    file.read(str, nbytes);
+    auto str = std::make_unique_for_overwrite<char[]>(nbytes);
+    file.read(str.get(), nbytes);
     // ::puts(str);
 
-    ::print(str);
+    ::print(str.get());
 
-    delete[] str;
     return EXIT_SUCCESS;
 }
