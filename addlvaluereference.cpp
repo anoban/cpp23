5724b56fd05fcf685e27e1adcaa65f2bd619fa6a
anoban <2016sb117@univ.jfn.ac.lk>
2024-12-22 17:40:54 +0530

    modified:   addlvaluereference.cpp 	modified:   swap.cpp

--- addlvaluereference.cpp
+++ addlvaluereference.cpp
@@ -1,29 +1,49 @@
 #include <type_traits>
 
-namespace dwyer {
+struct fallback_type final { };
+
+namespace dummy {
+
     template<typename _Ty, typename _TyPredicate> struct _add_lvref_helper final {
-            // PRIMARY TEMPLATE, INTENDED AS A FALLBACK FOR VOID TYPES
-            using type = _Ty;
+            using type = fallback_type; // PRIMARY TEMPLATE, INTENDED AS A FALLBACK FOR VOID TYPES
     };
 
     template<typename _Ty> struct _add_lvref_helper<_Ty, std::remove_reference_t<_Ty&>> final {
             using type = _Ty&;
     };
 
-    template<typename _Ty> using add_lvalue_reference_t = typename _add_lvref_helper<_Ty, _Ty>::type;
+    template<typename _Ty> using add_lvalue_reference_t = typename _add_lvref_helper<_Ty, std::remove_reference_t<_Ty>>::type;
 }
 
-static_assert(std::is_same_v<void, dwyer::add_lvalue_reference_t<void>>);
-static_assert(std::is_same_v<const void, dwyer::add_lvalue_reference_t<const void>>);
-static_assert(std::is_same_v<volatile void, dwyer::add_lvalue_reference_t<volatile void>>);
-static_assert(std::is_same_v<const volatile void, dwyer::add_lvalue_reference_t<const volatile void>>);
-static_assert(std::is_same_v<long&, dwyer::add_lvalue_reference_t<long&>>);
-static_assert(std::is_same_v<const float&, dwyer::add_lvalue_reference_t<const float>>);
+static_assert(std::is_same_v<::fallback_type, dummy::add_lvalue_reference_t<void>>);
+static_assert(std::is_same_v<::fallback_type, dummy::add_lvalue_reference_t<const void>>);
+static_assert(std::is_same_v<::fallback_type, dummy::add_lvalue_reference_t<volatile void>>);
+static_assert(std::is_same_v<::fallback_type, dummy::add_lvalue_reference_t<const volatile void>>);
+
+static_assert(std::is_same_v<long&, dummy::add_lvalue_reference_t<long&>>);
+static_assert(std::is_same_v<const float&, dummy::add_lvalue_reference_t<const float>>);
 
 static_assert(std::is_same_v<volatile double&, std::add_lvalue_reference_t<volatile double&&>>);
-static_assert(std::is_same_v<volatile double&, dwyer::add_lvalue_reference_t<volatile double&&>>); // WHY???
+static_assert(std::is_same_v<volatile double&, dummy::add_lvalue_reference_t<volatile double&&>>);
 
-static_assert(std::is_same_v<dwyer::_add_lvref_helper<volatile long&&, volatile long&&>::type, volatile long&>);
-static_assert(std::is_same_v<dwyer::_add_lvref_helper<long&, long>::type, long>);
+static_assert(std::is_same_v<dummy::_add_lvref_helper<volatile long&&, volatile long&&>::type, fallback_type>);
+static_assert(std::is_same_v<dummy::_add_lvref_helper<long&, long>::type, long&>);
 
-static_assert(std::is_same_v<const float&, dwyer::add_lvalue_reference_t<const float>>);
+static_assert(std::is_same_v<const float&, dummy::add_lvalue_reference_t<const float>>);
+
+namespace test {
+
+    template<class _Ty> struct bind final {
+            static constexpr bool is_bound_to_primary = true;
+    };
+
+    template<class _Ty> struct bind<_Ty&> final {
+            static constexpr bool is_bound_to_primary = false;
+    };
+}
+
+static_assert(test::bind<double>::is_bound_to_primary);
+static_assert(test::bind<double&&>::is_bound_to_primary); // in this case, if _Ty is deduced to be double&&
+// here the specialization could be used too i.e. double& + & = double&, but the primary is a better match than the specialization
+// in both cases, _Ty will be double&&
+static_assert(!test::bind<double&>::is_bound_to_primary);
--- swap.cpp
+++ swap.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #define __SSTRING_PRINT_METHOD_SIGNATURES_ON_CALL__ 1 // NOLINT
 #include <sstring>
 
@@ -13,6 +14,15 @@ static void swap(_Inout_::sstring& left, _Inout_::sstring& right) noexcept {
     right = std::move(temporary);            // move assignment
 }
 
+template<typename _Ty>
+std::ostream& operator<<(_Inout_ std::ostream& ostr, _In_ const std::vector<_Ty>& vect) noexcept(noexcept(std::cout << vect.at(0)))
+    requires requires { ostr << vect.at(0); } {
+    ostr << "{ ";
+    for (unsigned long long i = 0; i < vect.size() - 1; ++i) ostr << vect.at(i) << ", ";
+    ostr << vect.at(vect.size() - 1) << " }\n";
+    return ostr;
+}
+
 auto main() -> int {
     ::sstring hello { "world!\n" };
     ::sstring world { "Hello " };
@@ -25,5 +35,17 @@ auto main() -> int {
 
     ::_swap(hello, world);
     std::cout << hello << world;
+
+    std::vector<double> first { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    std::vector<double> second { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+    std::cout << first;
+    std::cout << second;
+
+    first.swap(second);
+
+    std::cout << first;
+    std::cout << second;
+
     return EXIT_SUCCESS;
 }
