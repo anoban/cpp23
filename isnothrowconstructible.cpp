577712ac71550614b84d30e68c31b37866da33c8
anoban <2016sb117@univ.jfn.ac.lk>
2024-12-23 05:55:37 +0530

    modified:   isnothrowconstructible.cpp

--- isnothrowconstructible.cpp
+++ isnothrowconstructible.cpp
@@ -1,3 +1,39 @@
+#include <sstring>
 #include <type_traits>
 
-namespace nstd { }
+namespace nstd {
+
+    template<typename _TyCandidate, typename _TyConstructed, typename... TyArgList> struct is_nothrow_constructible_from final {
+            static constexpr bool value = false;
+    };
+
+    template<typename _TyCandidate, typename... _TyArgList> struct is_nothrow_constructible_from<
+        _TyCandidate,
+        typename std::
+            enable_if_t<noexcept(_TyCandidate(std::declval<_TyArgList>()...)), decltype(_TyCandidate(std::declval<_TyArgList>()...))>,
+        _TyArgList...>
+        final {
+            static constexpr bool value = true;
+    };
+
+    template<typename _TyCandidate, typename... _TyArgList> static constexpr bool is_nothrow_constructible_from_v =
+        is_nothrow_constructible_from<_TyCandidate, _TyCandidate, _TyArgList...>::value;
+
+}
+
+struct may_throw final {
+        double value;
+
+        may_throw() : value() { } // a potentially throwing ctor
+
+        explicit may_throw(const double& v) noexcept : value(v) { } // a non-throwing ctor
+};
+
+static_assert(nstd::is_nothrow_constructible_from_v<::sstring>);
+static_assert(nstd::is_nothrow_constructible_from_v<::sstring, const char (&)[100]>);
+static_assert(nstd::is_nothrow_constructible_from_v<::sstring, std::string>);
+static_assert(!nstd::is_nothrow_constructible_from_v<::may_throw>);
+static_assert(nstd::is_nothrow_constructible_from_v<::may_throw, double>);
+static_assert(nstd::is_nothrow_constructible_from_v<::may_throw, const long&>);
+static_assert(!nstd::is_nothrow_constructible_from_v<::may_throw, const ::sstring>);
+static_assert(nstd::is_nothrow_constructible_from_v<::may_throw, double&&>);
