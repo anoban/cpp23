e67daa5c33e7f32ef41d4b6e1919bab3b81533e8
anoban <2016sb117@univ.jfn.ac.lk>
2025-01-01 17:45:02 +1100

    modified:   bool_constant.cpp

--- bool_constant.cpp
+++ bool_constant.cpp
@@ -48,3 +48,81 @@ template<typename _Ty> struct remove_reference<_Ty&&> final {
 static_assert(std::is_same_v<double, ::remove_reference<double&&>::type>);
 static_assert(std::is_same_v<const volatile float, ::remove_reference<const volatile float&>::type>);
 static_assert(std::is_same_v<volatile unsigned long, ::remove_reference<volatile unsigned long>::type>);
+
+template<typename _Ty> struct add_lvalue_reference final {
+        using type = _Ty&;
+};
+
+template<> struct add_lvalue_reference<void> final {
+        using type = void;
+};
+
+template<> struct add_lvalue_reference<const void> final {
+        using type = void;
+};
+
+template<> struct add_lvalue_reference<volatile void> final {
+        using type = void;
+};
+
+template<> struct add_lvalue_reference<const volatile void> final {
+        using type = void;
+};
+
+namespace alternative_00 {
+
+    template<typename _Ty, typename _TyDuplicate> struct _lvalue_reference_helper final {
+            using type = _Ty&;
+    };
+
+    template<typename _Ty> struct _lvalue_reference_helper<_Ty, void> final {
+            using type = _Ty;
+    };
+
+    template<typename _Ty> using add_lvalue_reference_t = typename _lvalue_reference_helper<_Ty, std::remove_cv<_Ty>::type>::type;
+
+}
+
+static_assert(std::is_same_v<void, alternative_00::add_lvalue_reference_t<void>>);
+static_assert(std::is_same_v<const void, alternative_00::add_lvalue_reference_t<const void>>);
+static_assert(std::is_same_v<volatile void, alternative_00::add_lvalue_reference_t<volatile void>>);
+static_assert(std::is_same_v<const volatile void, alternative_00::add_lvalue_reference_t<const volatile void>>);
+static_assert(std::is_same_v<float&, alternative_00::add_lvalue_reference_t<float&&>>);
+static_assert(std::is_same_v<const float&, alternative_00::add_lvalue_reference_t<const float>>);
+static_assert(std::is_same_v<volatile long long&, alternative_00::add_lvalue_reference_t<volatile long long&>>);
+
+namespace alternative_01 {
+
+    template<typename _Ty, bool _is_void> struct _lvalue_reference_helper final {
+            using type = _Ty&;
+    };
+
+    template<typename _Ty> struct _lvalue_reference_helper<_Ty, true> final {
+            using type = _Ty;
+    };
+
+    template<typename _Ty> using add_lvalue_reference_t = typename _lvalue_reference_helper<_Ty, std::is_void<_Ty>::value>::type;
+
+}
+
+static_assert(std::is_same_v<void, alternative_01::add_lvalue_reference_t<void>>);
+static_assert(std::is_same_v<const void, alternative_01::add_lvalue_reference_t<const void>>);
+static_assert(std::is_same_v<volatile void, alternative_01::add_lvalue_reference_t<volatile void>>);
+static_assert(std::is_same_v<const volatile void, alternative_01::add_lvalue_reference_t<const volatile void>>);
+static_assert(std::is_same_v<float&, alternative_01::add_lvalue_reference_t<float&&>>);
+static_assert(std::is_same_v<const float&, alternative_01::add_lvalue_reference_t<const float>>);
+static_assert(std::is_same_v<volatile long long&, alternative_01::add_lvalue_reference_t<volatile long long&>>);
+
+namespace alternative_02 {
+
+    template<typename _Ty, bool _is_void> struct _lvalue_reference_helper final {
+            using type = _Ty&;
+    };
+
+    template<typename _Ty> struct _lvalue_reference_helper<_Ty, true> final {
+            using type = _Ty;
+    };
+
+    template<typename _Ty> using add_lvalue_reference_t = typename _lvalue_reference_helper<_Ty, std::is_void<_Ty>::value>::type;
+
+}
