4fec8fb4b7dce1f7eef307f4a5df0b9bbbd30e1e
anoban <2016sb117@univ.jfn.ac.lk>
2025-01-01 17:30:25 +1100

    modified:   bool_constant.cpp

--- bool_constant.cpp
+++ bool_constant.cpp
@@ -1,13 +1,17 @@
 #include <type_traits>
 
-template<typename _Ty, _Ty _init> struct integral_constant {
-        static constexpr _Ty value = _init;
-};
-
 // using alias templates
-template<bool _init> using bool_constant = integral_constant<bool, _init>;
-using true_type                          = bool_constant<true>;
-using false_type                         = bool_constant<false>;
+namespace alias {
+
+    template<typename _Ty, _Ty _init> struct integral_constant final {
+            static constexpr _Ty value = _init;
+    };
+
+    template<bool _init> using bool_constant = integral_constant<bool, _init>;
+    using true_type                          = bool_constant<true>;
+    using false_type                         = bool_constant<false>;
+
+}
 
 // using inheritance
 namespace inheritance {
@@ -24,7 +28,23 @@ namespace inheritance {
 };
 
 // making progress :)
-static_assert(::true_type::value);
-static_assert(!::false_type::value);
+static_assert(alias::true_type::value);
+static_assert(!alias::false_type::value);
 static_assert(inheritance::true_type::value);
 static_assert(!inheritance::false_type::value);
+
+template<typename _Ty> struct remove_reference final {
+        using type = _Ty;
+};
+
+template<typename _Ty> struct remove_reference<_Ty&> final {
+        using type = _Ty;
+};
+
+template<typename _Ty> struct remove_reference<_Ty&&> final {
+        using type = _Ty;
+};
+
+static_assert(std::is_same_v<double, ::remove_reference<double&&>::type>);
+static_assert(std::is_same_v<const volatile float, ::remove_reference<const volatile float&>::type>);
+static_assert(std::is_same_v<volatile unsigned long, ::remove_reference<volatile unsigned long>::type>);
