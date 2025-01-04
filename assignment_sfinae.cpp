7c8de62b197a19cbe8cb5fcaa5d219277d309519
anoban <2016sb117@univ.jfn.ac.lk>
2025-01-05 09:08:46 +1100

    modified:   assignment_sfinae.cpp

--- assignment_sfinae.cpp
+++ assignment_sfinae.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <type_traits>
 
 // for an assignment to be valid, the types of right and left operands must be compatible
@@ -18,6 +19,24 @@ template<class _TyLeft, class _TyRight> static constexpr bool is_assignment_vali
 
 static_assert(::is_assignment_valid<double&, long long>);
 static_assert(::is_assignment_valid<double&, const long&&>);
+static_assert(::is_assignment_valid<double&, const double&>);
 static_assert(!::is_assignment_valid<const double, const long&&>);
 static_assert(!::is_assignment_valid<const double&, volatile short>);
 static_assert(::is_assignment_valid<volatile unsigned&, const long double&&>);
+static_assert(!::is_assignment_valid<volatile unsigned* const, volatile unsigned* const>);
+static_assert(::is_assignment_valid<const unsigned*&, const unsigned*>);
+
+int main() {
+    [[maybe_unused]] constexpr unsigned value { 785776 }, dummy { 7676 };
+    auto*                               ptr { &value };
+    ptr = &dummy;
+
+    auto&&         rvref { 657.0354 };
+    constexpr auto val { 0.767826 };
+    rvref = val;
+
+    [[maybe_unused]] constexpr auto isit =
+        std::is_same_v<decltype(std::declval<const double*&>() = std::declval<double*>()), const double*&>;
+    [[maybe_unused]] constexpr auto _isit = std::is_same_v<decltype(std::declval<double&>() = std::declval<const double&>()), double&>;
+    return EXIT_SUCCESS;
+}
