0b73bfc0129ac86bfe67fb4f6be18895291244df
anoban <2016sb117@univ.jfn.ac.lk>
2025-01-05 08:17:47 +1100

    new file:   assignment_sfinae.cpp

---
+++ assignment_sfinae.cpp
@@ -0,0 +1,23 @@
+#include <type_traits>
+
+// for an assignment to be valid, the types of right and left operands must be compatible
+// left operand must not be const
+// and the result of an assignment operation is always the type of the left operand
+
+template<class _TyLeft, class _TyRight, class _TyResult> struct __is_assignment_valid final {
+        static constexpr bool value = false; // fallback
+};
+
+template<class _TyLeft, class _TyRight>
+struct __is_assignment_valid<_TyLeft, _TyRight, decltype(std::declval<_TyLeft>() = std::declval<_TyRight>())> final {
+        static constexpr bool value = true; // for all the valid assignments
+};
+
+template<class _TyLeft, class _TyRight> static constexpr bool is_assignment_valid =
+    ::__is_assignment_valid<_TyLeft, _TyRight, _TyLeft>::value;
+
+static_assert(::is_assignment_valid<double&, long long>);
+static_assert(::is_assignment_valid<double&, const long&&>);
+static_assert(!::is_assignment_valid<const double, const long&&>);
+static_assert(!::is_assignment_valid<const double&, volatile short>);
+static_assert(::is_assignment_valid<volatile unsigned&, const long double&&>);
