ae874835b61ea86be87ba4dba5d814998abc59ce
anoban <2016sb117@univ.jfn.ac.lk>
2025-01-07 11:30:14 +1100

    modified:   _typededuction.cpp

--- _typededuction.cpp
+++ _typededuction.cpp
@@ -11,6 +11,13 @@ template<typename _Ty> static void references_only([[maybe_unused]] _Ty& _argume
     ::wprintf_s(L"Is _Ty a reference? %4s\n", std::is_reference_v<decltype(_argument)> ? L"Yes" : L"No");
 }
 
+template<typename _Ty> static void reference_types_only([[maybe_unused]] _Ty&& _univref) noexcept {
+    if constexpr (std::is_lvalue_reference_v<_Ty>)
+        ::_putws(L"_Ty is an lvalue reference");
+    else if constexpr (std::is_rvalue_reference_v<_Ty>)
+        ::_putws(L"_Ty is an rvalue reference");
+}
+
 int wmain() {
     constexpr auto value { std::numbers::pi_v<float> };
     const auto&    lvalue_reference { value };
