6d41e1736c1c464d24d3f2c7e0ec7b8556a87ac9
anoban <2016sb117@univ.jfn.ac.lk>
2025-01-19 20:44:09 +1100

    modified:   vtable.cpp

--- vtable.cpp
+++ vtable.cpp
@@ -20,6 +20,25 @@ static_assert(sizeof(base) == 8); // woohooo
 static_assert(sizeof(derived) == 8);
 
 int main() {
-    //
+    const derived     dobject {};
+    const base        bobject {};
+    const auto* const dptr { &dobject };
+    const auto* const bptr { &bobject };
+
+    // upcast
+    reinterpret_cast<const base*>(dptr)->hello(); // Hello from derived!
+
+    // downcast
+    reinterpret_cast<const derived* const>(bptr)->hello(); // Hello from base!
+
+    derived _derived {};
+    base    _base {};
+    // replace the _derived object's vtable pointer with class base's vtable pointer and see what happens when Hello() is called!
+    *reinterpret_cast<uintptr_t*>(&_derived) = *reinterpret_cast<const uintptr_t*>(&bobject);
+    *reinterpret_cast<uintptr_t*>(&_base)    = *reinterpret_cast<const uintptr_t*>(&dobject);
+
+    _derived.hello(); // Hello from base!
+    _base.hello();    // Hello from derived!
+
     return EXIT_SUCCESS;
 }
