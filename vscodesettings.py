c9cefdf89134f2b17fce8d24779cff0c00a7031c
anoban <2016sb117@univ.jfn.ac.lk>
2025-05-18 08:41:12 +1000

    modified:   vscodesettings.py

--- vscodesettings.py
+++ vscodesettings.py
@@ -1,11 +1,36 @@
 import json
+import os
+from typing import Any
 
 
-def __open_vscode_settings_dot_json(path: str) -> str:
-    with open(file=path, mode="r", encoding=r"ascii") as fptr:
-        vscode_settings = fptr.read()
-    return vscode_settings
+def open_vscode_settings_dot_json(path: os.PathLike) -> str:
+    """ """
+
+    try:
+        with open(file=path, mode="r", encoding=r"ascii") as fptr:
+            vscode_settings = fptr.read()
+        return vscode_settings
+    except FileNotFoundError as fnf_error:
+        raise RuntimeError(f"Could not find the specified settings.json file at {path}") from fnf_error
 
 
-def __parse_settings(settings: str) -> json:
-    pass
+def parse_settings(settings: str) -> dict[str, Any]:
+    """ """
+
+    try:
+        parsed_settings = json.loads(settings)
+        return parsed_settings
+    except json.decoder.JSONDecodeError as jsn_dcd_error:
+        raise RuntimeError("") from jsn_dcd_error
+
+
+def sort_settings(settings: dict[str, Any]) -> dict[str, Any]:
+    """ """
+
+    sorted_settings: dict[str, Any] = dict()
+    for key in sorted(settings):
+        if not isinstance(settings.get(key), dict):
+            sorted_settings[key] = settings.get(key)
+        else:
+            sorted_settings[key] = {k: settings.get(key).get(k) for k in sorted(settings.get(key))}
+    return sorted_settings
